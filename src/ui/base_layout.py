b6dfdde1c59c982b36886fb9ee6594b7b0529992
code-withmansoor <mansoormakandar262@gmail.com>
2026-08-02 15:21:48 +0530

    Finshsing touches

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -46,17 +46,24 @@ def style_base_layout():
           @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
           @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
   
+
                   
-           /* Hide Top Bar of streamlit */
-                            
-                        #MainMenu, footer, header {
-                            visibility: hidden;
-                        }
-                            
-                        .block-container {
-                            padding-top:1.5rem !important;    
-                        }
-  
+                   /* Hide Top Bar of streamlit */
+                          
+                      #MainMenu, footer, header {
+                          visibility: hidden;
+                      }
+                          
+                      .block-container {
+                          padding-top:1.5rem !important;    
+                      }
+          
+          
+          
+                        
+                                      
+                                                    
+                                                    
               h1 {
                   font-family: 'Climate Crisis', sans-serif !important;
                   font-size: 3.5rem !important;
